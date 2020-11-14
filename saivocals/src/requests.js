08a212ffecfb4603fb54409b10c2e908183a8b8a
saiwebcoding <74444496+saiwebcoding@users.noreply.github.com>
2020-11-14 12:41:50 +0530

    Improving Web by adding content

--- saivocals/src/requests.js
+++ saivocals/src/requests.js
@@ -8,6 +8,11 @@ const API_Key = '8a1ac56b3aa51c7a8722ffd5a93173c2';
 const requests = {
     fetchTrending:`/trending/all/week?api_key=${API_Key}&language=en-US`,
     fetchNetflixOriginals:`/discover/tv?api_key=${API_Key}&with_networks=213`,
+    fetchActionMovies:`/discover/movie?api_key=${API_Key}&with_genres=28`,
+    fetch:`/discover/movie?api_key=${API_Key}&with_genres=35`,
+    fetchActionMovies:`/discover/movie?api_key=${API_Key}&with_genres=27`,
+    fetchActionMovies:`/discover/movie?api_key=${API_Key}&with_genres=10749`,
+    fetchActionMovies:`/discover/movie?api_key=${API_Key}&with_genres=99`,
 }
 
 export default requests;
